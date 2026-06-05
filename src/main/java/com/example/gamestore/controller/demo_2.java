4430f4b2f08b9ea4abc5c3f4ab5f266041f792eb
feathers241 <trieule.727050@gmail.com>
2026-06-05 21:40:16 +0700

    Category

--- src/main/java/com/example/gamestore/controller/demo_2.java
+++ src/main/java/com/example/gamestore/controller/demo_2.java
@@ -5,16 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.gamestore.dao.CategoriesDao;
 import com.example.gamestore.dao.UserDao;
 
 @Controller
 public class demo_2 {
 	@Autowired
-	UserDao udao;
+	CategoriesDao cdao;
 	
 	@RequestMapping("/abc")
 	public String haha(Model m) {
-		m.addAttribute("a",udao.findAll());
+		m.addAttribute("a",cdao.findAll());
 		return "demo_2";
 	}
 }
--- src/main/java/com/example/gamestore/entity/Categories.java
+++ src/main/java/com/example/gamestore/entity/Categories.java
@@ -17,16 +17,16 @@ public class Categories {
 		this.category_name = category_name;
 		this.description = description;
 	}
-	public String getcategory_id() {
+	public String getCategory_id() {
 		return category_id;
 	}
-	public void setcategory_id(String category_id) {
+	public void setCategory_id(String category_id) {
 		this.category_id = category_id;
 	}
-	public String getcategory_name() {
+	public String getCategory_name() {
 		return category_name;
 	}
-	public void setcategory_name(String category_name) {
+	public void setCategory_name(String category_name) {
 		this.category_name = category_name;
 	}
 	public String getDescription() {
