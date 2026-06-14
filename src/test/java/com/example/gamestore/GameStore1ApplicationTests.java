087e7d66eb19a14da61b5126c8d09dafa02e2f44
Yuuki1606 <tranminhtriet52@gmail.com>
2026-06-14 16:18:28 +0700

    Triết

--- src/test/java/com/example/gamestore/GameStore1ApplicationTests.java
+++ src/test/java/com/example/gamestore/GameStore1ApplicationTests.java
@@ -1,18 +1,4 @@
-<<<<<<< HEAD:src/test/java/com/example/demo/GameStore1ApplicationTests.java
-//package com.example.demo;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class GameStore1ApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
-=======
+
 package com.example.gamestore;
 
 import org.junit.jupiter.api.Test;
@@ -26,4 +12,3 @@ class GameStore1ApplicationTests {
 	}
 
 }
->>>>>>> 52e6eaf290a8891b1d08a32ab1ea55b67e062762:src/test/java/com/example/gamestore/GameStore1ApplicationTests.java
