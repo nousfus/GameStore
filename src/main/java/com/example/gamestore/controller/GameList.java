99a3d4d615ac00c9d6ecd9631bca0b8d1491a670
feathers241 <trieule.727050@gmail.com>
2026-06-19 09:08:37 +0700

    Chức năng thêm vào giỏ hàng, thanh toán

--- src/main/java/com/example/gamestore/controller/GameList.java
+++ src/main/java/com/example/gamestore/controller/GameList.java
@@ -19,7 +19,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.example.gamestore.dao.CartDao;
 import com.example.gamestore.dao.CartItemsDao;
 import com.example.gamestore.dao.CategoriesDao;
+import com.example.gamestore.dao.DiscountsDao;
 import com.example.gamestore.dao.GameDao;
+import com.example.gamestore.dao.OrderDetailsDao;
+import com.example.gamestore.dao.OrdersDao;
+import com.example.gamestore.dao.PaymentsDao;
 import com.example.gamestore.entity.Cart;
 import com.example.gamestore.entity.CartItems;
 import com.example.gamestore.entity.Discounts;
@@ -43,6 +47,14 @@ public class GameList {
 	CartDao cartDao;
 	@Autowired
 	CartItemsDao cartitemdao;
+	@Autowired
+	OrdersDao orderdao;
+	@Autowired
+	OrderDetailsDao orderdetaildao;
+	@Autowired
+	DiscountsDao discountdao;
+	@Autowired
+	PaymentsDao paymentdao;
 	@GetMapping("/user/product-list")
 	public String productList(
 	        @RequestParam(defaultValue = "0") int page,
@@ -128,64 +140,66 @@ public class GameList {
 		}
 		return "forward:/user/cartitem/"+cartid;
 	}
-//	@PostMapping("/delete/{abc}")
-//	public String dleete(@PathVariable("abc")String id) {
-//		CartItems cart = cartitemsdao.findById(id).orElse(null);
-//		cartitemsdao.delete(cart);
-//		return "redirect:/list";
-//	}
-//	@PostMapping("/checkout")
-//	public String checkout(@RequestParam("pay") String pay, Model m) {
-//		String neworderid = null;
-//		for(Orders o : orderdao.findAll()) {
-//			int lastorder = Integer.parseInt(o.getOrder_id().substring(4));
-//			neworderid = "OD00"+(lastorder+1);									//Mã order
-//		}
-//		Date date = new Date(System.currentTimeMillis());
-//		
-//		String neededCart_id = "CART001";	//cart mẫu
-//		String tempusername = "admin01"; 	//username mẫu
-//		int total = 0;														//Tổng giá sản phẩm
-//		Game game = new Game();
-//		for(CartItems c : cartitemsdao.findAll()) {
-//			if(c.getCart_id().equals(neededCart_id)) {
-//				game = gamedao.findById(c.getGame().getGame_id()).orElse(null);
-//				total += game.getPrice() * c.getQuantity();
-//			}
-//		}
-//		Orders order = new Orders(neworderid,tempusername,date,total,"Pending");orderdao.save(order);	//tạo order
-//		
-//		List<OrderDetails> temp = orderdetaildao.findAll();
-//		OrderDetails od = temp.get(orderdetaildao.findAll().size()-1);
-//		int lastorder = Integer.parseInt(od.getOrder_detail_id().substring(5));
-//		String neworderdetailid = "ODD00"+(lastorder+1);									//Mã orderdetails
-//		
-//		Discounts discount = discountdao.findByGameId(game.getGame_id());					// Discount của game
-//		float discountamount = 0;
-//		for(CartItems c : cartitemsdao.findByCartId(neededCart_id)) {
-//			if(discount!=null) {
-//				discountamount = (c.getGame().getPrice() * discount.getdiscount_percent()) / 100;
-//			}else {discountamount = 0;}
-//			OrderDetails odd = new OrderDetails(neworderdetailid,order,c.getGame().getGame_id(),c.getGame().getPrice(),discountamount);orderdetaildao.save(odd);	
-//		}
-//		
-//		//Payments
-//		List<Payments> hahaha = paymentdao.findAll();
-//		Payments pa = hahaha.get(paymentdao.findAll().size()-1);
-//		
-//		int payment = Integer.parseInt(pa.getPayment_id().substring(4));
-//		String newpaymentid = "PM00"+(payment+1);									//Mã payment id
-//		
-//		int lsattrancation = Integer.parseInt(pa.getTransactionCode().substring(7));
-//		String newtransactionid = "TRANS00"+(lsattrancation+1);									//Mã transaction
-//		
-//		Payments payments = new Payments(newpaymentid,neworderid,pay,"Success",date,newtransactionid);paymentdao.save(payments);
-//		
-//		for(CartItems c : cartitemsdao.findAll()) {
-//			if(c.getCart_id().equals("neededCart_id")) {
-//				cartitemsdao.delete(c);
-//			}
-//		}
-//		return "forward:/list";
-//	}
+	@PostMapping("/delete/{abc}")
+	public String dleete(@PathVariable("abc")String id) {
+		CartItems cartitem = cartitemdao.findById(id).orElse(null);
+		String cartid = cartitem.getCartid();
+		cartitemdao.delete(cartitem);
+		return "redirect:/user/cartitem/"+cartid;
+	}
+	@PostMapping("/checkout")
+	public String checkout(@RequestParam("pay") String pay, Model m) {
+		String neworderid = null;
+		for(Orders o : orderdao.findAll()) {
+			int lastorder = Integer.parseInt(o.getOrder_id().substring(4));
+			neworderid = "OD00"+(lastorder+1);									//Mã order
+		}
+		Date date = new Date(System.currentTimeMillis());
+		
+		String neededCart_id = "CART001";	//cart mẫu
+		Users user = (Users) session.getAttribute("user");
+		String tempusername = user.getUsername(); 
+		int total = 0;														//Tổng giá sản phẩm
+		Game game = new Game();
+		for(CartItems c : cartitemdao.findAll()) {
+			if(c.getCartid().equals(neededCart_id)) {
+				game = gamedao.findById(c.getGame().getGame_id()).orElse(null);
+				total += game.getPrice() * c.getQuantity();
+			}
+		}
+		Orders order = new Orders(neworderid,tempusername,date,total,"Pending");orderdao.save(order);	//tạo order
+		
+		List<OrderDetails> temp = orderdetaildao.findAll();
+		OrderDetails od = temp.get(orderdetaildao.findAll().size()-1);
+		int lastorder = Integer.parseInt(od.getOrder_detail_id().substring(5));
+		String neworderdetailid = "ODD00"+(lastorder+1);									//Mã orderdetails
+		
+		Discounts discount = discountdao.findByGameId(game.getGame_id());					// Discount của game
+		float discountamount = 0;
+		for(CartItems c : cartitemdao.findByCartId(neededCart_id)) {
+			if(discount!=null) {
+				discountamount = (c.getGame().getPrice() * discount.getdiscount_percent()) / 100;
+			}else {discountamount = 0;}
+			OrderDetails odd = new OrderDetails(neworderdetailid,order,c.getGame().getGame_id(),c.getGame().getPrice(),discountamount);orderdetaildao.save(odd);	
+		}
+		
+		//Payments
+		List<Payments> hahaha = paymentdao.findAll();
+		Payments pa = hahaha.get(paymentdao.findAll().size()-1);
+		
+		int payment = Integer.parseInt(pa.getPayment_id().substring(4));
+		String newpaymentid = "PM00"+(payment+1);									//Mã payment id
+		
+		int lsattrancation = Integer.parseInt(pa.getTransactionCode().substring(7));
+		String newtransactionid = "TRANS00"+(lsattrancation+1);									//Mã transaction
+		
+		Payments payments = new Payments(newpaymentid,neworderid,pay,"Success",date,newtransactionid);paymentdao.save(payments);
+		
+		for(CartItems c : cartitemdao.findAll()) {
+			if(c.getCartid().equals("neededCart_id")) {
+				cartitemdao.delete(c);
+			}
+		}
+		return "forward:/user/product-list";
+	}
 }
