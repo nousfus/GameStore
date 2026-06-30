aec4a0e8cb6854535ca696540b94d97d576d7799
feathers241 <trieule.727050@gmail.com>
2026-06-30 19:59:09 +0700

    check

--- src/main/java/com/example/gamestore/controller/Quanlykhuyenmai.java
+++ src/main/java/com/example/gamestore/controller/Quanlykhuyenmai.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+// Test pull từ git hub
 @Controller 
 public class Quanlykhuyenmai {
 
